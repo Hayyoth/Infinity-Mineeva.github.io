1d9aa3fe1bf7e50c3479c601fd37cc098b69ef77
Sonji G <sonji.goodman@gmail.com>
2021-09-09 16:08:50 -0400

    Added new project to projects file

--- projects.js
+++ projects.js
@@ -1,42 +1,60 @@
 const projects = [
-   
-    {
-        id: 1,
-        projectImg: "images/Projects/Order-Summary-Card.jpg",
-        projectTitle: "Order Summary Card",
-        projectDesc: "Displaying an Order Summary Card that would display after a transaction.",
-        techUsed: {
-            icon1: "HTML",
-            icon2: "CSS"
-        },
-        demoLink: "http://Infinity-Mineeva.github.io/Order-Summary-Card/",
-        repoLink: "https://github.com/Infinity-Mineeva/Order-Summary-Card"
+  {
+    id: 1,
+    projectImg: "images/Projects/Order-Summary-Card.jpg",
+    projectTitle: "Order Summary Card",
+    projectDesc:
+      "Displaying an Order Summary Card that would display after a transaction.",
+    techUsed: {
+      icon1: "HTML",
+      icon2: "CSS",
     },
-    {
-        id: 2,
-        projectImg: "images/Projects/Apps_Layout.jpg",
-        projectTitle: "Top 5 Best Apps Template Clone",
-        projectDesc: "Clone of the 'Top 5 Best Apps' Template from leadpages.com",
-        techUsed: {
-            icon1: "HTML",
-            icon2: "CSS",
-            icon3: "JavaScript"
-        },
-        demoLink: "http://Infinity-Mineeva.github.io/Top-5-Best-Apps-Template-Clone/",
-        repoLink: "https://github.com/Infinity-Mineeva/Top-5-Best-Apps-Template-Clone"
+    demoLink: "http://Infinity-Mineeva.github.io/Order-Summary-Card/",
+    repoLink: "https://github.com/Infinity-Mineeva/Order-Summary-Card",
+  },
+  {
+    id: 2,
+    projectImg: "images/Projects/Apps_Layout.jpg",
+    projectTitle: "Top 5 Best Apps Template Clone",
+    projectDesc: "Clone of the 'Top 5 Best Apps' Template from leadpages.com",
+    techUsed: {
+      icon1: "HTML",
+      icon2: "CSS",
+      icon3: "JavaScript",
     },
-    {
-        id: 3,
-        projectImg: "images/Projects/DAVIDsTEA.jpg",
-        projectTitle: "DAVIDsTEA Promo Email Clone",
-        projectDesc: "A clone of a DAVIDsTEA's promotional email.",
-        techUsed: {
-            icon1: "HTML",
-            icon2: "CSS"
-        },
-        demoLink: "https://github.com/Infinity-Mineeva/DAVIDsTEA-promotional-email-clone",
-        repoLink: "https://github.com/Infinity-Mineeva/DAVIDsTEA-promotional-email-clone/index.html"
-    }
-]
+    demoLink:
+      "http://Infinity-Mineeva.github.io/Top-5-Best-Apps-Template-Clone/",
+    repoLink:
+      "https://github.com/Infinity-Mineeva/Top-5-Best-Apps-Template-Clone",
+  },
+  {
+    id: 3,
+    projectImg: "images/Projects/DAVIDsTEA.jpg",
+    projectTitle: "DAVIDsTEA Promo Email Clone",
+    projectDesc: "A clone of a DAVIDsTEA's promotional email.",
+    techUsed: {
+      icon1: "HTML",
+      icon2: "CSS",
+    },
+    demoLink:
+      "http://Infinity-Mineeva.github.io/DAVIDsTEA-promotional-email-clone",
+    repoLink:
+      "https://github.com/Infinity-Mineeva/DAVIDsTEA-promotional-email-clone",
+  },
+  {
+    id: 4,
+    projectImg: "images/Projects/Simple-Lead-Capture.jpg",
+    projectTitle: "Simple Lead Capture Landing Page Clone",
+    projectDesc:
+      "A for the Simple Lead Capture Landing Page Template found on the leadpages.com.",
+    techUsed: {
+      icon1: "HTML",
+      icon2: "CSS",
+      icon3: "JavaScript",
+    },
+    demoLink: "http://Infinity-Mineeva.github.io/Simple-Lead-Capture",
+    repoLink: "https://github.com/Infinity-Mineeva/Simple-Lead-Capture",
+  },
+];
 
-export  { projects };
+export { projects };
